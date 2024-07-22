cc43f3d7e291cc0303fb0869dcbd973c1da9189f
krk224 <hihas93@naver.com>
2024-07-22 18:50:22 +0900

    Time: 15 ms (36.18%), Space: 45.3 MB (50.36%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,26 +1,21 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        List<Person> pList = new ArrayList<>();
         int l = names.length;
-        for(int i =0; i< l; i++) {
-            pList.add(new Person(names[i], heights[i]));
-        }
-        Collections.sort(pList, (p1, p2) -> p2.height - p1.height);        
-        List<String> answer = new ArrayList<>();
-        for(int i =0; i< l; i++) {
-            answer.add(pList.get(i).name);
+        Map<Integer, String> map = new HashMap<>();
+        
+        for(int i =0; i < l; i++){
+            map.put(heights[i], names[i]);
         }
 
-        return answer.toArray(new String[0]);
-    }
-
-    public class Person {
-        public String name;
-        public int height;
-
-        public Person(String name, int height) {
-            this.name = name;
-            this.height = height;
+        List<Integer> hList = new ArrayList<>(Arrays.stream(heights).boxed().toList());
+        Collections.sort(hList, (h1, h2) -> h2 - h1);
+        List<String> nameList = new ArrayList<>();
+        
+        for(Integer h: hList) {
+            nameList.add(map.get(h));
         }
+        return nameList.toArray(new String[0]);
+
+        
     }
 }
