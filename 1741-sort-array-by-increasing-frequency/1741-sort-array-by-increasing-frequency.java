f8dddc82cae78a442e52bd7f77d6d2871f24e5bb
krk224 <hihas93@naver.com>
2024-07-23 19:11:23 +0900

    Time: 13 ms (8.57%), Space: 44.4 MB (59.67%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,35 +1,19 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, List<Integer>> resultMap = new HashMap<>();
+        List<Integer> result = new ArrayList<>(Arrays.stream(nums).boxed().toList());
         Map<Integer, Integer> cntMap = new HashMap<>();
         for(int i =0; i<nums.length;i++) {
             Integer currentCnt = cntMap.getOrDefault(nums[i], 0);
             cntMap.put(nums[i], ++currentCnt);
         }
-        for(Map.Entry<Integer, Integer> entry : cntMap.entrySet()) {
-            List<Integer> valueList = resultMap.get(entry.getValue());
-            if(valueList == null) {
-                valueList = new ArrayList<>();
-                valueList.add(entry.getKey());
-                resultMap.put(entry.getValue(), valueList);
-            } else {
-                valueList.add(entry.getKey());
+        Collections.sort(result, (a, b) -> {
+            if(cntMap.get(a).equals(cntMap.get(b))){
+                return Integer.compare(b,a);
             }
-        }
-        List<Integer> keyList = new ArrayList<>(resultMap.keySet());
-        Collections.sort(keyList);
-        for(Integer sortedKey : keyList) {
-            List<Integer> values = resultMap.get(sortedKey);
-            Collections.sort(values, (i1, i2) -> i2-i1);
-            for(Integer value : values) {
-                for(int i =0; i< sortedKey; i++) {
-                    result.add(value);
-                }
-            }
-            
-        }
-        return result.stream().mapToInt(i -> i).toArray();
+            return Integer.compare(cntMap.get(a), cntMap.get(b));
+        });
+
+        return result.stream().mapToInt(i->i).toArray();
         
     }
 }
