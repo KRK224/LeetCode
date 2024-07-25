7a734c399e26d771a3351853bbe5414653b1ef2c
krk224 <hihas93@naver.com>
2024-07-25 17:28:59 +0900

    Time: 2579 ms (5.02%), Space: 55.7 MB (63.65%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums);
+        return nums;
+
+    }
+
+    void mergeSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int tmp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && tmp < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = tmp;
+        }
+    }
+}
