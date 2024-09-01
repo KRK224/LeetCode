9b2d9cfdbee13445c0f72a86571086e5a08d9e62
krk224 <hihas93@naver.com>
2024-09-01 13:10:38 +0900

    Time: 5 ms (54.86%), Space: 55.3 MB (76.08%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -4,11 +4,13 @@ class Solution {
         int l = original.length;
         if(l != m*n)
             return new int[0][0];
+        
         for(int i = 0; i < m; i++) {
-            for (int j = 0; j< n; j++) {
-                int idx = i * n + j;
-                ans[i][j] = original[idx];
-            }
+            System.arraycopy(original, i*n, ans[i], 0, n);
+            // for (int j = 0; j< n; j++) {
+            //     int idx = i * n + j;
+            //     ans[i][j] = original[idx];
+            // }
         }
         return ans;
     }
