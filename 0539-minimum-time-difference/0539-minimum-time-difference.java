3b010b97f9b7555a5088268d01c07c3494c7799f
krk224 <hihas93@naver.com>
2024-09-16 18:55:58 +0900

    Time: 6 ms (55.09%), Space: 45.4 MB (63.14%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -5,28 +5,17 @@ class Solution {
         int[] minArr = new int[s];
 
         for(int i =0; i< s; i++) {
-            minArr[i] = calcMin(timePoints.get(i));
+            minArr[i] = Integer.parseInt(timePoints.get(i).substring(0,2)) * 60 + Integer.parseInt(timePoints.get(i).substring(3));
         }
 
         Arrays.sort(minArr);
 
         for(int i = 1; i < s; i++) {
-            answer = answer > minArr[i] - minArr[i-1] ? minArr[i] - minArr[i-1] : answer;
+            answer = Math.min(answer, minArr[i] - minArr[i-1]);
         }
-        int temp =24 * 60 - minArr[s -1] + minArr[0];
-        if(answer > temp) {
-            return temp;
-        } else {
-            return answer;
-        }
-
         
-    }
-
-    public int calcMin(String time) {
-        char[] hh_mm = time.toCharArray();
-        int hh = Character.getNumericValue(hh_mm[0]) * 10 + Character.getNumericValue(hh_mm[1]);
-        int mm = Character.getNumericValue(hh_mm[3]) *10 + Character.getNumericValue(hh_mm[4]);
-        return hh * 60 + mm;
+        answer = Math.min(answer, 24*60 - minArr[s-1] + minArr[0]);
+        
+        return answer;
     }
 }
