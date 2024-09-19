ea72588b5daab7e8133fafb152549df288cc5c1f
krk224 <hihas93@naver.com>
2024-09-19 17:35:03 +0900

    Time: 6 ms (54.53%), Space: 43.3 MB (48.05%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,41 +1,62 @@
 class Solution {
     public String largestNumber(int[] nums) {
         StringBuilder sb = new StringBuilder();
-        Map<String, List<String>> b = new HashMap<>();
+        List<String> l = new ArrayList();
 
-        // bucket initialized
-        for(int i : nums) {
-            String s = i + "";
-            List<String> l = b.getOrDefault(s.substring(0,1), new ArrayList<>());
-            l.add(s);
-            b.put(s.substring(0,1), l);
+        for (int i : nums) {
+            l.add(Integer.toString(i));
         }
 
-        for(int i = 9; i>= 0; i--) {
-            String key = i +"";
-            List<String> l = b.get(key);
-            if(l != null) {
-                System.out.println("key: " + key);
-                for(String e: l) {
-                    System.out.println(e);
-                }
-                l.sort((s1, s2) -> {
-                    String s1F = s1 + s2;
-                    String s2F = s2 + s1;
-                    return s2F.compareTo(s1F);
-                });
+        l.sort((s1, s2) -> {
+            return (s2 + s1).compareTo(s1 + s2);
+        });
 
-                for(String s: l) {
-                    sb.append(s);
-                }
-            }
-        }
-        
-        String ans = sb.toString();
-        if(ans.charAt(0)== '0') {
+        if (l.get(0).equals("0")) {
             return "0";
         }
-        return ans;
+
+        for (String e : l) {
+            sb.append(e);
+        }
+        return sb.toString();
 
     }
+    // Map<String, List<String>> b = new HashMap<>();
+
+    // // bucket initialized
+    // for(int i : nums) {
+    // String s = i + "";
+    // List<String> l = b.getOrDefault(s.substring(0,1), new ArrayList<>());
+    // l.add(s);
+    // b.put(s.substring(0,1), l);
+    // }
+
+    // for(int i = 9; i>= 0; i--) {
+    // String key = i +"";
+    // List<String> l = b.get(key);
+    // if(l != null) {
+    // System.out.println("key: " + key);
+    // for(String e: l) {
+    // System.out.println(e);
+    // }
+    // l.sort((s1, s2) -> {
+    // String s1F = s1 + s2;
+    // String s2F = s2 + s1;
+    // return s2F.compareTo(s1F);
+    // });
+
+    // for(String s: l) {
+    // sb.append(s);
+    // }
+    // }
+    // }
+
+    // String ans = sb.toString();
+    // if(ans.charAt(0)== '0') {
+    // return "0";
+    // }
+    // return ans;
+
+    // }
+
 }
