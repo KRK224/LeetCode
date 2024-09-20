4d5f6125b3bf5d934f596f82ac5ec68a68baf850
krk224 <hihas93@naver.com>
2024-09-20 23:46:49 +0900

    Time: 1 ms (100%), Space: 44.4 MB (92.98%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,19 +1,54 @@
+// class Solution {
+
+//     public String shortestPalindrome(String s) {
+//         int length = s.length();
+//         String reversedString = new StringBuilder(s).reverse().toString();
+
+//         // Iterate through the string to find the longest palindromic prefix
+//         for (int i = 0; i < length; i++) {
+//             if (
+//                 s.substring(0, length - i).equals(reversedString.substring(i))
+//             ) {
+//                 return new StringBuilder(reversedString.substring(0, i))
+//                     .append(s)
+//                     .toString();
+//             }
+//         }
+//         return "";
+//     }
+// }
+
 class Solution {
 
     public String shortestPalindrome(String s) {
         int length = s.length();
-        String reversedString = new StringBuilder(s).reverse().toString();
+        if (length == 0) {
+            return s;
+        }
 
-        // Iterate through the string to find the longest palindromic prefix
-        for (int i = 0; i < length; i++) {
-            if (
-                s.substring(0, length - i).equals(reversedString.substring(i))
-            ) {
-                return new StringBuilder(reversedString.substring(0, i))
-                    .append(s)
-                    .toString();
+        // Find the longest palindromic prefix
+        int left = 0;
+        for (int right = length - 1; right >= 0; right--) {
+            if (s.charAt(right) == s.charAt(left)) {
+                left++;
             }
         }
-        return "";
+
+        // If the whole string is a palindrome, return the original string
+        if (left == length) {
+            return s;
+        }
+
+        // Extract the suffix that is not part of the palindromic prefix
+        String nonPalindromeSuffix = s.substring(left);
+        StringBuilder reverseSuffix = new StringBuilder(
+            nonPalindromeSuffix
+        ).reverse();
+
+        // Form the shortest palindrome by prepending the reversed suffix
+        return reverseSuffix
+            .append(shortestPalindrome(s.substring(0, left)))
+            .append(nonPalindromeSuffix)
+            .toString();
     }
 }
