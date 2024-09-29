da1e1e5b042828b2eb9a0be95165cb30d0100d92
krk224 <hihas93@naver.com>
2024-09-29 20:05:56 +0900

    Time: 2 ms (98.68%), Space: 44.8 MB (47.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,41 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[] {
-            -1, -1
-        };
+        // Brute force
+        // int[] answer = new int[] {
+        // -1, -1
+        // };
 
-        for (int i =0; i< nums.length; i++) {
-            for(int j =i+1; j<nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    break;
-                }
-            }
-            
-            if(answer[1] != -1) {
+        // for (int i =0; i< nums.length; i++) {
+        // for(int j =i+1; j<nums.length; j++) {
+        // if(nums[i] + nums[j] == target) {
+        // answer[0] = i;
+        // answer[1] = j;
+        // break;
+        // }
+        // }
+
+        // if(answer[1] != -1) {
+        // break;
+        // }
+        // }
+        // return answer;
+        // }
+
+        // one-pass hash table
+        int[] answer = new int[2];
+        Map<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            Integer idx = hm.get(target-nums[i]);
+            if(idx != null) {
+                answer[0] = i;
+                answer[1] = idx;
                 break;
             }
+            hm.put(nums[i], i);
         }
+
         return answer;
+
     }
 }
