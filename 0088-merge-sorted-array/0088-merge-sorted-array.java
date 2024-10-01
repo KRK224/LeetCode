956e295b800a631eb24b99d526e415cd26d3ac24
krk224 <hihas93@naver.com>
2024-10-01 23:39:38 +0900

    Time: 0 ms (100%), Space: 42.2 MB (29.4%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // 배열 복사
-        for(int i =0; i< n; i++) {
-            nums1[m+i] = nums2[i];
+        // // 배열 복사
+        // for(int i =0; i< n; i++) {
+        //     nums1[m+i] = nums2[i];
+        // }
+        // merge(nums1, 0, m+n -1);
+
+         int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
-        merge(nums1, 0, m+n -1);
         
     }
 
