9d52dfc61bfe3403c23ca5935701906dde007c4f
krk224 <hihas93@naver.com>
2024-10-02 22:23:02 +0900

    Time: 22 ms (13.89%), Space: 45.1 MB (19.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,32 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int f = 0, e = s.length()-1;
-        while (f < e) {
-            while(!Character.isLetterOrDigit(s.charAt(f))) {
-                f++;
-                if(f>=s.length())
-                    return true;
-            }
+        // replaceAll doesn't change origin!!!
+        String letter = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        System.out.println(letter);
+        System.out.println(s);
+        String reverse = new StringBuilder(letter).reverse().toString().toLowerCase();
+        if(letter.equals(reverse))
+            return true;
+        return false;
+        // int f = 0, e = s.length()-1;
+        // while (f < e) {
+        //     while(!Character.isLetterOrDigit(s.charAt(f))) {
+        //         f++;
+        //         if(f>=s.length())
+        //             return true;
+        //     }
 
-            while(!Character.isLetterOrDigit(s.charAt(e))){
-                e--;
-                if(e<0)
-                    return true;
-            }
+        //     while(!Character.isLetterOrDigit(s.charAt(e))){
+        //         e--;
+        //         if(e<0)
+        //             return true;
+        //     }
 
-            if(Character.toLowerCase(s.charAt(f)) != Character.toLowerCase(s.charAt(e)))
-                return false;
-            f++;
-            e--;
-        }
-        return true;
+        //     if(Character.toLowerCase(s.charAt(f)) != Character.toLowerCase(s.charAt(e)))
+        //         return false;
+        //     f++;
+        //     e--;
+        // }
+        // return true;
     }
 }
