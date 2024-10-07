7282a7eb859a38ffc1b0e860c99476a8b3bbc9bc
krk224 <hihas93@naver.com>
2024-10-07 16:51:39 +0900

    Time: 18 ms (7.85%), Space: 45.2 MB (10.81%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -12,7 +12,6 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         for(int i =0; i< size; i++) {
-            System.out.println("i: " + i);
             if(i == size-1) {
                 sb.append(s.charAt(i));
             } else if((s.charAt(i) == 'A' && s.charAt(i+1) == 'B')||(s.charAt(i) == 'C' && s.charAt(i+1) == 'D'))  {
@@ -22,6 +21,5 @@ class Solution {
             }
         }
         return sb.toString();  
-
     }
 }
