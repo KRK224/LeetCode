7d12b5ce039422168850b9419d1aa222a6d6f7da
krk224 <hihas93@naver.com>
2024-10-07 17:26:24 +0900

    Time: 4 ms (85%), Space: 43 MB (91.72%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,20 +1,35 @@
 class Solution {
-
-
+    
     public int minLength(String s) {
-        // Continue processing while "AB" or "CD" substrings exist
-        while (s.contains("AB") || s.contains("CD")) {
-            if (s.contains("AB")) {
-                // Remove all occurrences of "AB"
-                s = s.replace("AB", "");
-            } else if (s.contains("CD")) {
-                // Remove all occurrences of "CD"
-                s = s.replace("CD", "");
+        Stack<Character> stack = new Stack<>();
+
+        // Iterate over each character in the input string
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            // If the stack is empty, simply push the current character
+            if (stack.isEmpty()) {
+                stack.push(currentChar);
+                continue;
+            }
+
+            // Check for "AB" pattern, remove the pair by popping from the stack
+            if (currentChar == 'B' && stack.peek() == 'A') {
+                stack.pop();
+            }
+            // Check for "CD" pattern, remove the pair by popping from the stack
+            else if (currentChar == 'D' && stack.peek() == 'C') {
+                stack.pop();
+            }
+            // Otherwise, push the current character onto the stack
+            else {
+                stack.push(currentChar);
             }
         }
 
-        return s.length();
+        return stack.size();
     }
+    
     // public int minLength(String s) {
     //     String ans = s;
     //     do {
