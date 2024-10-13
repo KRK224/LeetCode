50d0b149ecb5fd409a500282523491628f33114d
krk224 <hihas93@naver.com>
2024-10-13 21:00:33 +0900

    Time: 18 ms (31.1%), Space: 42.9 MB (56.21%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.java
+++ 0837-most-common-word/0837-most-common-word.java
@@ -1,18 +1,17 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         String answer = "";
-        String r = paragraph.replaceAll("[^A-Za-z]", " ").toLowerCase();
+        Set<String> bans = new HashSet<>(Arrays.asList(banned));
+        String r = paragraph.replaceAll("[^A-Za-z ]", " ").toLowerCase();
         String[] words = r.split("\\s+");
         Map<String, Integer> map = new HashMap<>();
 
         for(String w : words) {
             Integer cnt = map.getOrDefault(w, 0);
-            map.put(w, ++cnt);
+            if(!bans.contains(w))
+                map.put(w, ++cnt);
         }
-        
-        for(String b : banned) {
-            map.remove(b);
-        }
+    
 
         int maxCnt =0;
         for(Map.Entry<String, Integer> entry: map.entrySet()) {
