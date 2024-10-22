3a8119097e1cb51362fe03e8a00eecb6937a8d34
krk224 <hihas93@naver.com>
2024-10-22 18:14:57 +0900

    Time: 4 ms (99.73%), Space: 60.5 MB (7.19%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -17,11 +17,18 @@ class Solution {
             }
         }
 
-        dp(0, 0);
         int max = 0;
-        for(int i =0; i<m; i++) {
-            for (int j =0; j< n; j++) {
-                max = Math.max(max, dp[i][j]);
+
+        for(int i =m-1; i>=0; i--) {
+            for (int j =n-1; j>=0; j--) {
+                if(dp[i][j] ==-1) {
+                    if(matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i+1][j], dp[i][j+1]), dp[i+1][j+1]) +1;
+                    max = Math.max(dp[i][j], max);
+                    } else {
+                        dp[i][j] =0;
+                    }
+                }                
             }
         }       
 
@@ -29,20 +36,21 @@ class Solution {
     }
 
     // i,j 가 최상단 왼쪽인 경우에 만들 수 있는 최대 크기의 한 변 길이
-    public int dp(int i, int j) {
-        // memoization이 안된 경우 계산
-        if(dp[i][j] == -1) {
-            if(matrix[i][j] == '0') {
-                dp[i][j] = 0;
-                dp(i+1, j);
-                dp(i, j+1);
-            }                
-            else {
-                int l = Math.min(Math.min(dp(i+1, j), dp(i, j+1)), dp(i+1, j+1)) +1;
-                dp[i][j] = l;
-            }
-        }
+    // public int dp(int i, int j) {
+    //     // memoization이 안된 경우 계산
+    //     if(dp[i][j] == -1) {
+    //         if(matrix[i][j] == '0') {
+    //             dp[i][j] = 0;
+    //             // 계속 호출을 안해서.. 임의로; 근데 중복이 많아져서 그냥 for문으로 bottom up 하는게 더 좋을 듯
+    //             dp(i+1, j);
+    //             dp(i, j+1);
+    //         }                
+    //         else {
+    //             int l = Math.min(Math.min(dp(i+1, j), dp(i, j+1)), dp(i+1, j+1)) +1;
+    //             dp[i][j] = l;
+    //         }
+    //     }
 
-        return dp[i][j];
-    }
+    //     return dp[i][j];
+    // }
 }
