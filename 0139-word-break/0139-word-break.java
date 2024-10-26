0046d670591ac018fd405878bc850737c0853f56
krk224 <hihas93@naver.com>
2024-10-26 17:31:31 +0900

    Time: 3 ms (82.35%), Space: 42.3 MB (75.31%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -5,10 +5,11 @@ class Solution {
         dp = new boolean[s.length()];
 
         for (int i = 0; i < s.length(); i++) {
-            if (!dp[i]) {
-                for (String word : wordDict) {
-                    if (i >= word.length() - 1 && (i == word.length() - 1 || dp[i - word.length()])) {
-                        dp[i] = s.substring(i - word.length() + 1, i + 1).equals(word) || dp[i];
+            for (String word : wordDict) {
+                if (i >= word.length() - 1 && (i == word.length() - 1 || dp[i - word.length()])) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
                     }
                 }
             }
