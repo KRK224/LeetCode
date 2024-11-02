29dd5d0dac9e9c458d4fdc634348a28721695278
krk224 <hihas93@naver.com>
2024-11-02 22:55:37 +0900

    Time: 35 ms (72.91%), Space: 44.6 MB (21.9%) - LeetHub

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    // 가장 큰 값이 왜 peek이 아니지..?
+
+    public int minimumMountainRemovals(int[] nums) {
+        int N = nums.length;
+
+        int[] lisLength = new int[N];
+        int[] ldsLength = new int[N];
+
+        Arrays.fill(lisLength, 1);
+        Arrays.fill(ldsLength, 1);
+
+        // Stores the length of longest increasing subsequence that ends at i.
+        for (int i = 0; i < N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    lisLength[i] = Math.max(lisLength[i], lisLength[j] + 1);
+                }
+            }
+        }
+
+        // Stores the length of longest decreasing subsequence that starts at i.
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = i + 1; j < N; j++) {
+                if (nums[i] > nums[j]) {
+                    ldsLength[i] = Math.max(ldsLength[i], ldsLength[j] + 1);
+                }
+            }
+        }
+
+        int minRemovals = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            if (lisLength[i] > 1 && ldsLength[i] > 1) {
+                minRemovals = Math.min(
+                    minRemovals,
+                    N - lisLength[i] - ldsLength[i] + 1
+                );
+            }
+        }
+
+        return minRemovals;
+    }
+}
