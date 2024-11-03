9e60c1f2abebe6551378544f3dc7acad59dcc983
krk224 <hihas93@naver.com>
2024-11-03 14:21:27 +0900

    Time: 0 ms (100%), Space: 41.6 MB (20.3%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         boolean ans = false;
+        if(s.length() != goal.length())
+            return false;
         if(goal.equals(s))
             return true;
 
-        String reorder = new String(goal);
-        int length = s.length();
+        String doubleString = s + s;
 
-        while(length >0) {
-            reorder = reorder.substring(1) + reorder.substring(0,1);
-            // System.out.println(reorder);
-            if(reorder.equals(s))
-                return true;
-            length--;
-        }
+        if(doubleString.contains(goal))
+            return true;
 
         return false;
         
